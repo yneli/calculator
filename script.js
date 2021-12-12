11713ce5a417b11a7a4f18a2baa50151e15715ac
yneli <Mr.RadReIII@yandex.ru>
2021-12-12 14:39:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+
+let a = '';
+let b = '';
+let sign = '';
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'x','/'];
+const out = document.querySelector('.calc-screen p');
+
+function clearAll(){
+    a = '';
+    b = '';
+    sign = '';
+    finish = false;
+    out.textContent = 0;
+}
+
+document.querySelector('.ac').onclick = clearAll;
+
+
+document.querySelector('.buttons').onclick = (event) => {
+
+    if(!event.target.classList.contains('btn')) return;
+
+    if(event.target.classList.contains('ac')) return;
+
+    out.textContent = '';
+    const key = event.target.textContent;
+
+    if (digit.includes(key)) {
+        if (b === '' && sign === ''){
+            a+=key;
+            out.textContent = a;
+
+        }
+        else if (a !=='' && b !=='' && finish ){
+        }
+        else {
+            b += key;
+            out.textContent = b;
+        }
+        return
+    }
+    if (action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        return;
+    }
+
+    if (key === '='){
+        if (b === '') b = a;
+        switch (sign){
+            case "+":
+                a = (+a) + (+b);
+                break;
+            case "-":
+                a = a - b;
+                break;
+            case "x":
+                a = a * b;
+                break;
+            case "/":
+                if (b === '0'){
+                    out.textContent = 'ERROR'
+                    a = '';
+                    b = '';
+                    sign = '';
+                    return
+                }
+                a = a / b;
+                break;
+            
+        }
+        finish = true;
+        out.textContent = a;
+
+    }
+}
+
